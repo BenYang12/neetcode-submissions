c695d64819e35b4eb5fa15910f2d54cb34a420f5
Ben Yang <yangbenjamin2005@gmail.com>
2026-06-10 14:56:44 -0500

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #index 1 and index 2 cannot be equal => may not use the same element twice
+        #solution must use O(1) additional space
+        #Classic Two Pointer  (While L < R)
+
+        L = 0
+        R = len(numbers) - 1
+
+        while L < R:
+            curSum = numbers[L] + numbers[R]
+
+            if curSum > target:
+                R-=1
+            elif curSum < target:
+                L += 1
+            else:
+                return [L + 1, R + 1]
+           
+            
+
+           
+    
+        

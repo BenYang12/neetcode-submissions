b0e3aa1259096f174e410c461e802fea5dfe2939
Ben Yang <yangbenjamin2005@gmail.com>
2026-07-22 14:52:47 -0500

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        #create adj list {course: [prerequisites]}
+        prereq = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre) 
+
+        # a course has three possible states
+        # visited -> crs has been added to output
+        # visiting -> crs not added to output, but added to cycle
+        # unvisited -> crs not added to output or cycle
+        output = []
+        visit, cycle = set(), set()
+        def dfs(crs):
+            #detect cycle
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+
+            #recursively run dfs
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+
+        for c in range(numCourses):
+            if dfs(c) == False:
+                return []
+        return output
